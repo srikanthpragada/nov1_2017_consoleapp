8bc2d64079358bea053a17d18bbea84deca8e05f
srikanthpragada <srikanthpragada@gmail.com>
2017-11-03 09:30:48 +0530

    3-NOV

---
+++ ArrayDemo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class ArrayDemo
+    {
+        static void Main(string[] args)
+        {
+            int [] marks = { 79, 88, 69, 48, 78 ,88,65 };
+
+
+            for (int i = 0; i < marks.Length ; i++)
+                Console.WriteLine(marks[i]);
+
+            foreach (int m in marks)
+                Console.WriteLine(m);
+
+
+        }
+    }
+}
---
+++ FunctionsDemo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class FunctionsDemo
+    {
+        public static void Main()
+        {
+            int a = 10, b = 20;
+
+            Swap(ref a,ref b);
+
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+
+            int num;
+
+            Zero(out num);
+
+            Console.WriteLine(num);
+        }
+
+        public static void Zero(out int v)
+        {
+             v = 0; 
+        }
+
+        public static void Swap(ref int n1, ref int n2)
+        {
+            int temp = n1;
+
+            n1 = n2;
+            n2 = temp; 
+        }
+
+        public static int Add(int n1, int n2)
+        {
+            return n1 + n2; 
+        }
+    }
+}
